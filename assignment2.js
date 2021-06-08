3f8a6a6b8fa06633cd75079a9c3080e80ccb094e
yumandee <yumandee@yahoo.com>
2021-06-08 11:42:20 -0400

    myEach & myPush implemented

--- assignment2.js
+++ assignment2.js
@@ -5,8 +5,11 @@ Modified by Mandy Yu
 */
 
 // FOR EACH //
-Array.prototype.myEach = function() {
-
+Array.prototype.myEach = function(callbackFn) {
+    for (let i = 0; i < this.length; i++) {
+        if(this[i] === undefined) continue;
+        callbackFn(this[i], i, this);
+    }
 };
 
 // MAP //
@@ -45,8 +48,16 @@ Array.prototype.myIndexOf = function() {
 };
 
 // PUSH //
-Array.prototype.myPush = function() {
+Array.prototype.myPush = function(...args) {
+    let arg_i = 0;
+    let length = this.length;
 
+    for(let i = length; i < length + args.length; i++) {
+        this[i] = args[arg_i];
+        arg_i++;
+    }
+
+    return this.length;
 };
 
 // LASTINDEXOF //
@@ -63,4 +74,26 @@ Object.grabKeys = function() {
 Object.grabValues = function() {
 
 };
+
+/*
+TESTING
+*/
+
+// const isEven = (number) => console.log(number % 2 === 0);
+// let temp0 = [1, 2, 3, 4];
+// console.log("forEach:");
+// temp0.forEach(isEven);
+// console.log("myEach:");
+// temp0.myEach(isEven);
+
+
+// console.log("push:");
+// let temp1 = [1, 2, 3, 4];
+// temp1.push(5, 6, 7, 8);
+// console.log(temp1);
+
+// console.log("myPush:");
+// let temp2 = [1, 2, 3, 4];
+// temp2.myPush(5, 6, 7, 8);
+// console.log(temp2);
 
