7882db726bd7c137a34e4232eb517014294a1c6b
yumandee <yumandee@yahoo.com>
2021-06-07 21:59:51 -0400

    Create assignment2.js

---
+++ assignment2.js
@@ -0,0 +1,66 @@
+/*
+Practical Web Dev CSci 39548
+Assignment 2 
+Modified by Mandy Yu
+*/
+
+// FOR EACH //
+Array.prototype.myEach = function() {
+
+};
+
+// MAP //
+Array.prototype.myMap = function() {
+
+};
+
+// FILTER //
+Array.prototype.myFilter = function() {
+
+};
+
+// SOME //
+Array.prototype.mySome = function() {
+
+};
+
+// EVERY //
+Array.prototype.myEvery = function() {
+
+};
+
+// REDUCE //
+Array.prototype.myReduce = function() {
+
+};
+
+// INCLUDES //
+Array.prototype.myIncludes = function() {
+
+};
+
+// INDEXOF //
+Array.prototype.myIndexOf = function() {
+
+};
+
+// PUSH //
+Array.prototype.myPush = function() {
+
+};
+
+// LASTINDEXOF //
+Array.prototype.myLastIndexOf = function() {
+
+};
+
+// KEYS //
+Object.grabKeys = function() {
+
+};
+
+// VALUES //
+Object.grabValues = function() {
+
+};
+
