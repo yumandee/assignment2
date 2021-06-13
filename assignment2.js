f503f17be48f97c946f93c704b14dcb1c17ec873
yumandee <yumandee@yahoo.com>
2021-06-13 01:11:18 -0400

    code cleanup

--- assignment2.js
+++ assignment2.js
@@ -44,60 +44,13 @@ Array.prototype.mySome = function(callbackFn) {
 // EVERY //
 Array.prototype.myEvery = function(callbackFn) {
     let length = this.length;
-    for(let i = 0; i <length; i++) {
+    for(let i = 0; i < length; i++) {
         if(this[i] === undefined) continue;
         if(!callbackFn(this[i], i, this)) return false;
     }
     return true;
 };
 
-// ---------------
-// Modifying items
-// ---------------
-let arr = [1, 2, 3, 4];
-arr.myEvery( (elem, index, arr) => {
-  arr[index+1] -= 1
-  console.log(`[${arr}][${index}] -> ${elem}`)
-  return elem < 2
-})
-
-// Loop runs for 3 iterations, but would
-// have run 2 iterations without any modification
-//
-// 1st iteration: [1,1,3,4][0] -> 1
-// 2nd iteration: [1,1,2,4][1] -> 1
-// 3rd iteration: [1,1,2,3][2] -> 2
-
-// ---------------
-// Appending items
-// ---------------
-arr = [1, 2, 3];
-arr.myEvery( (elem, index, arr) => {
-  arr.push('new')
-  console.log(`[${arr}][${index}] -> ${elem}`)
-  return elem < 4
-})
-
-// Loop runs for 3 iterations, even after appending new items
-//
-// 1st iteration: [1, 2, 3, new][0] -> 1
-// 2nd iteration: [1, 2, 3, new, new][1] -> 2
-// 3rd iteration: [1, 2, 3, new, new, new][2] -> 3
-// ---------------
-// Deleting items
-// ---------------
-arr = [1, 2, 3, 4];
-arr.myEvery( (elem, index, arr) => {
-  arr.pop()
-  console.log(`[${arr}][${index}] -> ${elem}`)
-  return elem < 4
-})
-
-// Loop runs for 2 iterations only, as the remaining
-// items are `pop()`ed off
-//
-// 1st iteration: [1,2,3][0] -> 1
-// 2nd iteration: [1,2][1] -> 2
 // REDUCE //
 Array.prototype.myReduce = function(callbackFn, initialValue) {
     let accumulator = 0;
@@ -109,6 +62,7 @@ Array.prototype.myReduce = function(callbackFn, initialValue) {
     }
     return accumulator;
 };
+
 
 // INCLUDES //
 Array.prototype.myIncludes = function(searchElement, fromIndex = 0) {
